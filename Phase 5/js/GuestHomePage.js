eed34560151cbc9556d21911257cefbdbcc54f3f
WaleedBader <hurywaleed22@gmail.com>
2024-11-07 18:59:32 +0300

    ReportDetails for Guest & modifications on styles, (Guest Pages Finished)

--- Phase 5/js/GuestHomePage.js
+++ Phase 5/js/GuestHomePage.js
@@ -2,6 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const sidebar = document.querySelector(".sidebar");
     const sidebarMenu = document.querySelector(".sidebar-menu");
     const loginButton = document.querySelector(".login-btn"); // Select the LOGIN button
+    const searchButton = document.querySelector(".search-btn"); // Select the Search button
+    
+    document.addEventListener("click", (event) => {
+        if (event.target.classList.contains("details-btn")) {
+            window.location.href = "ReportDetailsGuest.html"; // Redirect to ReportDetailsGuest.html
+        }
+    });
 
     // Toggle the sidebar expanded class when the menu button or sidebar is clicked
     sidebarMenu.addEventListener("click", () => {
@@ -10,6 +17,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Redirect to the Login page when the LOGIN button is clicked
     loginButton.addEventListener("click", () => {
-        window.location.href = "../Login/Login.html"; // Adjust the path as needed
+        window.location.href = "Login.html"; // Adjust the path as needed
+    });
+
+    // Redirect to the SearchPageGuest.html when the Search button is clicked
+    searchButton.addEventListener("click", () => {
+        window.location.href = "SearchPageGuest.html"; // Adjust the path as needed
     });
 });
