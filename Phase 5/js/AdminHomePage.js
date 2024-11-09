5f74213e3ecc9118194ae31c3a783b2095b90045
WaleedBader <hurywaleed22@gmail.com>
2024-11-09 13:28:26 +0300

    All Pages ...

---
+++ Phase 5/js/AdminHomePage.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sidebar = document.querySelector(".sidebar");
+    const sidebarMenu = document.querySelector(".sidebar-menu");
+    const searchButton = document.querySelector(".search-btn");
+    const lostButton = document.querySelector(".report-lost-btn");
+    const foundButton = document.querySelector(".report-found-btn"); 
+    
+    document.addEventListener("click", (event) => {
+        if (event.target.classList.contains("details-btn")) {
+            window.location.href = "AdminApprovedReport.html";
+        }
+    });
+     
+
+    // Toggle the sidebar expanded class when the menu button or sidebar is clicked
+    sidebarMenu.addEventListener("click", () => {
+        sidebar.classList.toggle("sidebar-expanded");
+    });
+
+    
+    // Redirect to the SearchPageGuest.html when the Search button is clicked
+    searchButton.addEventListener("click", () => {
+        window.location.href = "SearchPageAdmin.html"; 
+    });
+
+    const profileIcon = document.querySelector(".profile-icon");
+    profileIcon.addEventListener("click", () => {
+        window.location.href = "ProfileAdmin.html";
+    });
+
+    // Redirect to ParticipantNotifications.html when the notification icon is clicked
+    const notificationIcon = document.querySelector(".notification-icon");
+    notificationIcon.addEventListener("click", () => {
+        window.location.href = "AdminNotifications.html";
+    });
+});
