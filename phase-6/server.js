4b533a6bbfe95f1994ea417cf902d416812d6973
Alhussain-PC <alhussain1234@gmail.com>
2024-12-10 22:09:27 +0300

    major changes for deploying

--- phase-6/server.js
+++ phase-6/server.js
@@ -42,8 +42,5 @@ app.use(notFound); // Handles undefined routes
 // Error handling middleware
 app.use(errorHandler);
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// Export the app for Vercel
+module.exports = app;
